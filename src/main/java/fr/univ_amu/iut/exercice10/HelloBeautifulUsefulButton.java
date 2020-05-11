176b52101bfacd28710d1974866d24d0c8cb28e5
iDz0 <iDzeed144@gmail.com>
2020-05-11 17:31:23 +0200

    Commit de l'exo 10

--- src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
+++ src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
@@ -1,6 +1,13 @@
 package fr.univ_amu.iut.exercice10;
 
+import fr.univ_amu.iut.exercice9.EcouteurSimple;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloBeautifulUsefulButton extends Application {
@@ -8,6 +15,37 @@ public class HelloBeautifulUsefulButton extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Hello !");
+
+        Button button = new Button("Hello !");
+        button.setId("buttonHello");
+
+        EventHandler<ActionEvent> ecouteur = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                nbAction += 1;
+
+                System.out.println("Bouton actionné "+ nbAction +" fois");
+            }
+        };
+
+        button.setOnAction(ecouteur);
+
+
+
+        ImageView iv = new ImageView("https://raw.githubusercontent.com/IUTInfoAix-M2105/Syllabus/master/assets/logo.png");
+        button.setGraphic(iv);
+
+        BorderPane BP = new BorderPane(button);
+        //BP.setAlignment(label, Pos.CENTER);
+        BP.setPrefSize(250, 250);
+
+        Scene s = new Scene(BP);
+        s.getStylesheets().add(getClass().getClassLoader().getResource("DarkTheme.css").toExternalForm());
+        primaryStage.setScene(s);
+
+        primaryStage.show();
 
     }
 }
