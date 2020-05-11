abde2a70e6d0ad60e65d5745ad41c5062243e32a
iDz0 <iDzeed144@gmail.com>
2020-05-11 16:39:40 +0200

    Commit de l'exo 7

--- src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
+++ src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
@@ -1,11 +1,28 @@
 package fr.univ_amu.iut.exercice7;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloButton extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        primaryStage.setTitle("Hello !");
+
+        Button button = new Button("Hello !");
+        button.setId("buttonHello");
+
+        BorderPane BP = new BorderPane(button);
+        //BP.setAlignment(label, Pos.CENTER);
+        BP.setPrefSize(250, 100);
+
+        Scene s = new Scene(BP);
+        primaryStage.setScene(s);
+
+        primaryStage.show();
     }
 }
